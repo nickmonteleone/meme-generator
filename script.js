2a225babe23e232e657828f1ad04b3a9259a6a00
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-10-26 18:52:36 -0700

    refactor script js

--- script.js
+++ script.js
@@ -1,54 +1,51 @@
+// get constants for element ids
+const inputImageFile = document.getElementById("input-image-file");
+const inputTopText = document.getElementById("input-top-text");
+const inputBottomText = document.getElementById("input-bottom-text");
+// get main section to append box to
+const mainPage = document.getElementById('main-page');
 // add event listener for form and init counter for memes
 const form = document.querySelector("form");
 let memesCreated = 0;
 form.addEventListener("submit", generateMeme);
 
-// script for adding meme when button clicked
 function generateMeme(event) {
-  // prevent default so page doesn't reload
+  /* add a new meme to the page */
   event.preventDefault();
-  // get inputs from form using dom
-  const inputImageFile = document.getElementById("input-image-file");
-  const inputTopText = document.getElementById("input-top-text");
-  const inputBottomText = document.getElementById("input-bottom-text");
-  // get result message dom
-  const inputResultText = document.getElementById('input-result-text');
   // check if image and/or text was provided
   let imageProvided = inputImageFile.files[0] != undefined;
   let textProvided = inputTopText.value.length > 0 || inputBottomText.value.length > 0;
-  // get main section to append box to
-  const mainPage = document.getElementById('main-page');
-  // create meme box with number of meme
-  const memeBox = document.createElement('div');
-  memeBox.classList.add('meme-box');
-  // create elements for the meme image, text, and close button
-  const memeImage = document.createElement("img");
-  memeImage.classList.add('meme-image');
-  const memeTopText = document.createElement("div");
-  memeTopText.classList.add('meme-text', 'top-text');
-  const memeBottomText = document.createElement("div");
-  memeBottomText.classList.add('meme-text', 'bottom-text');
-  const memeClose = document.createElement('button');
-  memeClose.classList.add('btn-close', 'meme-close');
-  // append children elements to meme box
-  memeBox.appendChild(memeImage);
-  memeBox.appendChild(memeTopText);
-  memeBox.appendChild(memeBottomText);
-  memeBox.appendChild(memeClose);
-  // update the meme image if file provided
-  if (imageProvided) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      memeImage.src = e.target.result;
-    };
-    reader.readAsDataURL(inputImageFile.files[0]);
-  }
-  else memeImage.src = "images/dat-boi-bike.gif";
-  // update the meme text
-  memeTopText.innerText = inputTopText.value;
-  memeBottomText.innerText = inputBottomText.value;
   // when image or text provided, append new meme box to main page
   if (imageProvided || textProvided) {
+    // create elements for the meme
+    const memeBox = document.createElement('div');
+    memeBox.classList.add('meme-box');
+    const memeImage = document.createElement("img");
+    memeImage.classList.add('meme-image');
+    const memeTopText = document.createElement("div");
+    memeTopText.classList.add('meme-text', 'top-text');
+    const memeBottomText = document.createElement("div");
+    memeBottomText.classList.add('meme-text', 'bottom-text');
+    const memeClose = document.createElement('button');
+    memeClose.classList.add('btn-close', 'meme-close');
+    // add src for image, use input if file provided
+    if (imageProvided) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        memeImage.src = e.target.result;
+      };
+      reader.readAsDataURL(inputImageFile.files[0]);
+    }
+    else memeImage.src = "images/dat-boi-bike.gif";
+    // update the meme text
+    memeTopText.innerText = inputTopText.value;
+    memeBottomText.innerText = inputBottomText.value;
+    // append children elements to meme box
+    memeBox.appendChild(memeImage);
+    memeBox.appendChild(memeTopText);
+    memeBox.appendChild(memeBottomText);
+    memeBox.appendChild(memeClose);
+    // set box id based on how many memes on page
     memesCreated++;
     memeBox.setAttribute('id', `meme-${memesCreated}`);
     // add event listener for close button
@@ -64,13 +61,21 @@ function generateMeme(event) {
       const lastMeme = document.getElementsByClassName("meme-box")[0];
       mainPage.insertBefore(memeBox, lastMeme);
     }
-    // clear form fields if meme generated
-    inputImageFile.value = "";
-    inputTopText.value = "";
-    inputBottomText.value = "";
+    resetInputs();
   }
-  console.log(textProvided)
-  // set result message based on what was provieded
+  setResultMessage(imageProvided, textProvided);
+}
+
+function resetInputs() {
+  // clear form fields if meme generated
+  inputImageFile.value = "";
+  inputTopText.value = "";
+  inputBottomText.value = "";
+}
+
+function setResultMessage(imageProvided, textProvided) {
+  // set result message based on what was provided
+  const inputResultText = document.getElementById('input-result-text');
   if (imageProvided && textProvided) {
     inputResultText.innerText = 'Nice meme!\nRetry steps 1-4 to make another.';
   }
